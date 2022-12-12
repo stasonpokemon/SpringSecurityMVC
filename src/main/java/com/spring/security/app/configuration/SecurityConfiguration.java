1cd6b5c0afbf936eef1ed8243883aac7c7b96e41
Stanislau Trebnikau <stasonpokemon@icloud.com>
2022-12-12 18:49:16 +0300

    Custom Authentication form;

--- src/main/java/com/spring/security/app/configuration/SecurityConfiguration.java
+++ src/main/java/com/spring/security/app/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -18,13 +19,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected PersonDetailsServiceImpl personDetailsService;
 
 
+    // Authentications configure
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
 
+    // Spring Security configure
+    // Authorization configure
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/auth/login", "/error").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .formLogin().loginPage("/auth/login")
+                    .loginProcessingUrl("/process_login")
+                    .defaultSuccessUrl("/hello", true)
+                    .failureUrl("/auth/login?error");
+    }
+
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
+    public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 
